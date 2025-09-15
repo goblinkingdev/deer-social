f00c77ba620e21153f3e965d3cb93cae542d64a4
GoblinKing <68219518+goblinkingdev@users.noreply.github.com>
2025-09-15 18:39:02 +0000

    Final fix svg

--- src/components/icons/Statics.tsx
+++ src/components/icons/Statics.tsx
@@ -1,5 +1,5 @@
 import {createSinglePathSVG} from './TEMPLATE'
 
 export const Statics_Stroke2_Corner0_Rounded = createSinglePathSVG({
-  path: 'M 160 -160 v -320 h 160 v 320 H 160 Z m 240 0 v -640 h 160 v 640 H 400 Z m 240 0 v -440 h 160 v 440 H 640 Z',
+  path: 'M6 4a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h2zm7 4a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h2zm7-4a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h2z',
 })
