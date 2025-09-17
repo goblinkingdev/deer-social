271a385573947faff0f63422a7938c277355d6e0
ayla <ayla-git.barcode041@silomails.com>
2025-09-17 18:43:48 -0300

    dumbass

--- src/lib/strings/headings.ts
+++ src/lib/strings/headings.ts
@@ -1,4 +1,4 @@
 export function bskyTitle(page: string, unreadCountLabel?: string) {
   const unreadPrefix = unreadCountLabel ? `(${unreadCountLabel}) ` : ''
-  return `${unreadPrefix}${page} — deer-social-ayla.pages.dev`
+  return `${unreadPrefix}${page} — Deer`
 }
