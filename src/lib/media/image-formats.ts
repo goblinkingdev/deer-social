ecf2f354fda48b7dfa3b1eaf5507c7f75e468d8b
ayla <ayla-git.barcode041@silomails.com>
2025-09-24 18:08:55 -0300

    more formats

--- src/lib/media/image-formats.ts
+++ src/lib/media/image-formats.ts
@@ -1,3 +1,5 @@
+import {isNative} from '#/platform/detection'
+
 export const IMAGE_FORMATS = [
   {label: 'AVIF', value: 'avif'},
   {label: 'WebP', value: 'webp'},
@@ -5,4 +7,6 @@ export const IMAGE_FORMATS = [
   {label: 'PNG', value: 'png'},
   {label: 'GIF', value: 'gif'},
   {label: 'BMP', value: 'bmp'},
+  {label: 'ICO', value: 'ico'},
+  ...(isNative ? [{label: 'HEIC', value: 'heic'}] : []),
 ]
