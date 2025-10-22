514a4f895f9482e2d1a4b28e8e35f3bfd33f0700
GoblinKing <68219518+goblinkingdev@users.noreply.github.com>
2025-10-22 19:55:34 +0000

    si

--- modules/bottom-sheet/src/BottomSheet.web.tsx
+++ modules/bottom-sheet/src/BottomSheet.web.tsx
@@ -1,5 +1,14 @@
 import {type BottomSheetViewProps} from './BottomSheet.types'
 
+// Diagnostic logging for debugging Babel scope tracker issue
+// This will help us understand the module loading behavior
+console.log('DEBUG: BottomSheet.web.tsx module loaded')
+console.log(
+  'DEBUG: Available exports from BottomSheet.types:',
+  Object.keys(require('./BottomSheet.types')),
+)
+
 export function BottomSheet(_: BottomSheetViewProps) {
+  console.log('DEBUG: BottomSheet function called')
   throw new Error('BottomSheet is not available on web')
 }
